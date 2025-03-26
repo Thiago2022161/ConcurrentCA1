55ac00ef77e9c64ce964ccbde9f12c297f24600d
Thiago2022161 <2022161@student.cct.ie>
2025-03-26 19:48:57 +0000

    Added DataReader class to load CSV file
    Created the DataReader class in the csv package. It reads all numbers from csv file and stores them in a double array, which might be used later. The file is placed in the root folder of the project.

--- src/csv/DataReader.java
+++ src/csv/DataReader.java
@@ -4,10 +4,28 @@
  */
 package csv;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author thiagogoncos
  */
 public class DataReader {
     
+    public static double[] readCSV(String path) throws IOException {
+        List<Double> numbers = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            String[] values = line.split(",");
+            for (String val : values) {
+                numbers.add(Double.parseDouble(val.trim()));
+            }
+        }
+
+        reader.close();
+        return numbers.stream().mapToDouble(d -> d).toArray();
+    } 
 }
