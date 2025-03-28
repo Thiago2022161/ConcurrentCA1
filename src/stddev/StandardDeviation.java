b4570e63edecbeb880be4d9716aaf34c31978c53
Thiago2022161 <2022161@student.cct.ie>
2025-03-28 18:20:41 +0000

    Add StandardDeviation class
    Created the StandardDeviation class in the stddev package. It uses ForkJoin to calculate the squared differences from the mean, which is needed to find the standard deviation using concurrency.

--- src/stddev/StandardDeviation.java
+++ src/stddev/StandardDeviation.java
@@ -4,10 +4,45 @@
  */
 package stddev;
 
+import java.util.concurrent.RecursiveTask;
+
 /**
  *
  * @author thiagogoncos
  */
-public class StandardDeviation {
-    
+
+public class StandardDeviation extends RecursiveTask<Double> {
+
+    private final double[] data;
+    private final int start, end;
+    private final double mean;
+    private static final int THRESHOLD = 20;
+
+    public StandardDeviation(double[] data, int start, int end, double mean) {
+        this.data = data;
+        this.start = start;
+        this.end = end;
+        this.mean = mean;
+    }
+
+    @Override
+    protected Double compute() {
+        if (end - start <= THRESHOLD) {
+            double sum = 0;
+            for (int i = start; i < end; i++) {
+                sum += Math.pow(data[i] - mean, 2);
+            }
+            return sum;
+        } else {
+            int mid = (start + end) / 2;
+            StandardDeviation leftTask = new StandardDeviation(data, start, mid, mean);
+            StandardDeviation rightTask = new StandardDeviation(data, mid, end, mean);
+
+            leftTask.fork(); 
+            double rightResult = rightTask.compute(); 
+            double leftResult = leftTask.join(); 
+
+            return leftResult + rightResult;
+        }
+    }
 }
