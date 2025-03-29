b0ab4a3f2de8241310f0c5e2bbcfc3a89600c0b8
Thiago2022161 <2022161@student.cct.ie>
2025-03-29 09:30:46 +0000

    Created ConcurrentRunner to run all tasks with concurrency
    Added the main class ConcurrentRunner in the concurrentca1 package. It runs all three tasks using concurrency:
    - Calculates standard deviation using ForkJoin
    - Multiplies two matrices using threads
    - Sorts all 200 values in descending order using ForkJoin merge sort
    Also improved output readability by printing results clearly, including line breaks every 10 sorted values.

--- src/concurrentca1/ConcurrentRunner.java
+++ src/concurrentca1/ConcurrentRunner.java
@@ -4,10 +4,54 @@
  */
 package concurrentca1;
 
+import csv.DataReader;
+import stddev.StandardDeviation;
+import matrix.MatrixCalculation;
+import sort.DataSorter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+
 /**
  *
  * @author thiagogoncos
  */
+
 public class ConcurrentRunner {
-    
+    public static void main(String[] args) throws IOException {
+
+        double[] data = DataReader.readCSV("data.csv");
+
+        double mean = Arrays.stream(data).average().orElse(0);
+        ForkJoinPool pool = new ForkJoinPool();
+        double squaredSum = pool.invoke(new StandardDeviation(data, 0, data.length, mean));
+        double stdDev = Math.sqrt(squaredSum / data.length);
+        System.out.printf("Standard Deviation: %.4f\n\n", stdDev);
+
+        int size = 10;
+        double[][] matrixA = new double[size][size];
+        double[][] matrixB = new double[size][size];
+
+        for (int i = 0; i < size; i++) {
+            System.arraycopy(data, i * size, matrixA[i], 0, size);
+            System.arraycopy(data, (i + size) * size, matrixB[i], 0, size);
+        }
+
+        double[][] resultMatrix = MatrixCalculation.multiply(matrixA, matrixB);
+        System.out.println("Matrix (10x10) Multiplication Result:\n");
+        for (double[] row : resultMatrix) {
+            System.out.println(Arrays.toString(row));
+        }
+
+        double[] dataCopy = Arrays.copyOf(data, data.length);
+        pool.invoke(new DataSorter(dataCopy, 0, dataCopy.length - 1));
+        System.out.println("\nAll 200 Numbers Sorted (Descending):\n");
+        for (int i = 0; i < dataCopy.length; i++) {
+    System.out.print(dataCopy[i] + "\t");
+    if ((i + 1) % 10 == 0) {
+        System.out.println(); 
+    }
+}
+    }
 }
