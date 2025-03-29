668649ac70d3e538535022556ec391224a9ae112
Thiago2022161 <2022161@student.cct.ie>
2025-03-29 07:36:31 +0000

    Add MatrixCalculation class for concurrent multiplication
    Created the MatrixCalculation class in the matrix package. It multiplies two 10x10 matrices using threads. Each thread calculates one row of the result matrix. This makes the multiplication faster using concurrency.

--- src/matrix/MatrixCalculation.java
+++ src/matrix/MatrixCalculation.java
@@ -8,6 +8,34 @@ package matrix;
  *
  * @author thiagogoncos
  */
+
 public class MatrixCalculation {
-    
+
+    public static double[][] multiply(double[][] a, double[][] b) {
+        int size = a.length;
+        double[][] result = new double[size][size];
+        Thread[] threads = new Thread[size];
+
+        for (int i = 0; i < size; i++) {
+            final int row = i;
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < size; j++) {
+                    for (int k = 0; k < size; k++) {
+                        result[row][j] += a[row][k] * b[k][j];
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
 }
