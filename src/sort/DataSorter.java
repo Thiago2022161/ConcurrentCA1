8b37a02bca0a2939b5c63814019a46e49b831355
Thiago2022161 <2022161@student.cct.ie>
2025-03-29 08:34:23 +0000

    Added DataSorter class with parallel merge sort (descending)
    Created the DataSorter class in the sort package. It uses ForkJoin and merge sort to sort all 200 values from the CSV file in descending order. The merge was changed to put the biggest numbers first. This uses concurrency to make sorting faster.

--- src/sort/DataSorter.java
+++ src/sort/DataSorter.java
@@ -4,10 +4,52 @@
  */
 package sort;
 
+import java.util.concurrent.RecursiveAction;
+
 /**
  *
  * @author thiagogoncos
  */
-public class DataSorter {
-    
+
+public class DataSorter extends RecursiveAction {
+
+    private final double[] array;
+    private final int left, right;
+
+    public DataSorter(double[] array, int left, int right) {
+        this.array = array;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    protected void compute() {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            DataSorter leftTask = new DataSorter(array, left, mid);
+            DataSorter rightTask = new DataSorter(array, mid + 1, right);
+
+            invokeAll(leftTask, rightTask);
+            merge(left, mid, right);
+        }
+    }
+
+    private void merge(int left, int mid, int right) {
+        double[] temp = new double[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (array[i] > array[j]) {
+                temp[k++] = array[i++];
+            } else {
+                temp[k++] = array[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = array[i++];
+        while (j <= right) temp[k++] = array[j++];
+
+        System.arraycopy(temp, 0, array, left, temp.length);
+    }
 }
